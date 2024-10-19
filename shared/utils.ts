78a87d3f91b59c3d56c8ee5aaa251780260ceb92
PalmDevs <git@palmdevs.me>
2024-10-19 09:11:15 +0700

    feat(shared/utils): add `inspect` function

--- shared/utils.ts
+++ shared/utils.ts
@@ -2,3 +2,5 @@ import { findByPropsLazy } from '@revenge-mod/metro'
 import { proxyLazy } from '@revenge-mod/utils/lazy'
 
 export const findPropLazy = (prop: string) => proxyLazy(() => findByPropsLazy(prop)[prop])
+
+export const inspect = findPropLazy('inspect')
