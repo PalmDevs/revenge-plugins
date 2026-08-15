2f274513157d37727e929a7bae18a3a3d28c1ae4
PalmDevs <git@palmdevs.me>
2026-08-15 15:33:46 +0700

    chore: update project

--- modules.d.ts
+++ modules.d.ts
@@ -1,34 +1,34 @@
 declare module '@revenge-mod/api' {
-    const api: typeof import('@revenge-mod/revenge/src/lib/api')
-    export * from '@revenge-mod/revenge/src/lib/api'
+    const api: typeof import('./node_modules/@revenge-mod/revenge/src/lib/api')
+    export * from './node_modules/@revenge-mod/revenge/src/lib/api'
     export default api
 }
 
 declare module '@revenge-mod/metro' {
-    const metro: typeof import('@revenge-mod/revenge/src/metro')
-    export * from '@revenge-mod/revenge/src/metro'
+    const metro: typeof import('./node_modules/@revenge-mod/revenge/src/metro')
+    export * from './node_modules/@revenge-mod/revenge/src/metro'
     export default metro
 }
 
 declare module '@revenge-mod/metro/common' {
-    const common: typeof import('@revenge-mod/revenge/src/metro/common')
-    export * from '@revenge-mod/revenge/src/metro/common'
+    const common: typeof import('./node_modules/@revenge-mod/revenge/src/metro/common')
+    export * from './node_modules/@revenge-mod/revenge/src/metro/common'
     export default common
 }
 
 declare module '@revenge-mod/ui' {
-    const ui: typeof import('@revenge-mod/revenge/src/lib/ui')
-    export * from '@revenge-mod/revenge/src/lib/ui'
+    const ui: typeof import('./node_modules/@revenge-mod/revenge/src/lib/ui')
+    export * from './node_modules/@revenge-mod/revenge/src/lib/ui'
     export default ui
 }
 declare module '@revenge-mod/utils' {
-    const utils: typeof import('@revenge-mod/revenge/src/lib/utils')
-    export * from '@revenge-mod/revenge/src/lib/utils'
+    const utils: typeof import('./node_modules/@revenge-mod/revenge/src/lib/utils')
+    export * from './node_modules/@revenge-mod/revenge/src/lib/utils'
     export default utils
 }
 
 declare module '@revenge-mod/utils/lazy' {
-    export const lazy: typeof import('@revenge-mod/revenge/src/lib/utils/lazy')
-    export * from '@revenge-mod/revenge/src/lib/utils/lazy'
+    export const lazy: typeof import('./node_modules/@revenge-mod/revenge/src/lib/utils/lazy')
+    export * from './node_modules/@revenge-mod/revenge/src/lib/utils/lazy'
     export default lazy
 }
